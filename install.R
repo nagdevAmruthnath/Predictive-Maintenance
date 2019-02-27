5e720c505eebea17e313200719a2c603dcdd6345
Nagdev Amruthnath <33914037+nagdevAmruthnath@users.noreply.github.com>
2019-02-27 16:35:02 -0500

    Update install.R

--- install.R
+++ install.R
@@ -1,1 +1,2 @@
-install.packages(c(qcc"))
+install.packages(c("qcc","caret", "foreach", "dplyr", "h2o", "doParallel", "ModelMetrics", "mclust"))
+install.packages('mdatools',  dependencies=TRUE , version='0.9.1')
