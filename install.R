0f56338f8a5493a5379ef0a3f3f042922711d2ed
Nagdev Amruthnath <33914037+nagdevAmruthnath@users.noreply.github.com>
2020-01-15 14:36:13 -0500

    Update install.R
    removed an extra comma

--- install.R
+++ install.R
@@ -1,2 +1,2 @@
-install.packages(c("qcc","caret", "foreach", "dplyr", "h2o", "doParallel", "ModelMetrics", "mclust", "e1071","imports","randomForest", "keras", ), dependencies=TRUE)
+install.packages(c("qcc","caret", "foreach", "dplyr", "h2o", "doParallel", "ModelMetrics", "mclust", "e1071","imports","randomForest", "keras" ), dependencies=TRUE)
 install.packages('https://cran.r-project.org/src/contrib/mdatools_0.9.1.tar.gz')
